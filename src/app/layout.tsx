4f643cbeaec9970e02ff0e7effd652f05083304a
Ismael Strey Pereira <ismaelstrey@hotmail.com>
2026-03-10 23:17:42 -0300

    Suppress hydration warning on root html/body (#63)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <StyledComponentsRegistry>
           <StyledComponentsProvider>
             <AuthProvider>
